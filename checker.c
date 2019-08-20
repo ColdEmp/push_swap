e3435825108ebb6e2764eecd90885a2e9baab2a9
Cameron Glanville <cglanvil@c3r6s7.wethinkcode.co.za>
2019-08-20 13:55:23 +0200

    updated checker to accept negative numbers

--- checker.c
+++ checker.c
@@ -6,52 +6,13 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 11:02:49 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/08/19 19:16:15 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/08/20 13:54:18 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	test(int *table, int len)
-{
-	int	i;
-
-	i = 1;
-	while (i < len)
-	{
-		if (table[i] < table[i - 1])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	checker(char **array)
-{
-	int	i;
-	int	len;
-	int	*table;
-
-	len = 0;
-	i = 0;
-	while (array[len] != '\0')
-		len++;
-	if (!(table = (int*)malloc(sizeof(int) * len)))
-		return (-1);
-	while (array[i] != '\0')
-	{
-		if ((table[i] = ft_atoi(array[i])) == (int)NULL)
-			return (-1);
-		i++;
-	}
-	if (len == 1)
-		return (1);
-	if (!test(table, len))
-		return (0);
-	return (1);
-}
-
-int	test_ilist(t_ilist *head)
+void	test_ilist(t_ilist *head)
 {
 	t_ilist *temp;
 
@@ -59,10 +20,10 @@ int	test_ilist(t_ilist *head)
 	while (temp->next)
 	{
 		if (temp->nbr > (temp->next)->nbr)
-			return (0);
+			KO;
 		temp = temp->next;
 	}
-	return (1);
+	OK;
 }
 
 t_ilist	*ilstnew(int nbr)
@@ -72,7 +33,7 @@ t_ilist	*ilstnew(int nbr)
 	if (!(temp = (t_ilist*)malloc(sizeof(t_ilist))))
 		return (NULL);
 	if (!(temp->nbr = (int)malloc(sizeof(int))))
-		return(NULL);
+		return (NULL);
 	temp->nbr = nbr;
 	temp->next = NULL;
 	return (temp);
@@ -89,21 +50,23 @@ void	check_error(char **array)
 		j = 0;
 		while (array[i][j])
 		{
-			if (ft_isdigit(array[i][j]) != 1)
-				error_exit;
+			if ((j == 0 && ft_isdigit(array[i][j]) != 1 && array[i][j] != '-')
+					|| (j == 0 && array[i][j] == '-' &&
+					ft_isdigit(array[i][j + 1]) != 1)
+					|| (j > 0 && ft_isdigit(array[i][j]) != 1))
+				ERROR_EXIT;
 			j++;
 		}
 		i++;
 	}
 }
 
-int	check_ilist(char **array)
+void	check_ilist(char **array)
 {
 	t_ilist	*head;
 	t_ilist	*temp;
 	int		i;
 
-	i = 0;
 	check_error(array);
 	head = ilstnew(ft_atoi(array[0]));
 	temp = head;
@@ -114,12 +77,10 @@ int	check_ilist(char **array)
 		temp = temp->next;
 		i++;
 	}
-	if (!test_ilist(head))
-		return (0);
-	return (1);
+	test_ilist(head);
 }
 
-int	main(int argc, char *argv[])
+int		main(int argc, char *argv[])
 {
 	char	**array;
 
@@ -128,18 +89,10 @@ int	main(int argc, char *argv[])
 		if (argc == 2)
 		{
 			array = ft_strsplit(argv[1], ' ');
-			if (check_ilist(array) == 1)
-				ft_putstr("OK\n");
-			else
-				ft_putstr("KO\n");
+			check_ilist(array);
 		}
 		else
-		{
-			if (check_ilist(argv + 1) == 1)
-				ft_putstr("OK\n");
-			else
-				ft_putstr("KO\n");
-		}
+			check_ilist(argv + 1);
 	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cglanvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 15:22:10 by cglanvil          #+#    #+#             */
-/*   Updated: 2019/08/19 19:16:12 by cglanvil         ###   ########.fr       */
+/*   Updated: 2019/08/20 13:30:13 by cglanvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define PUSH_SWAP_H
 # include "libft/libft.h"
 # define DUP_NUMS {ft_putstr("Duplicate numbers\n"); exit(1);}
-# define error_exit {ft_putstr("ERROR\n"); exit(1);}
+# define ERROR_EXIT {ft_putstr("ERROR\n"); exit(1);}
+# define OK {ft_putstr("OK\n"); exit(1);}
+# define KO {ft_putstr("KO\n"); exit(1);}
+
 typedef struct	s_ilist
 {
 	int				nbr;
